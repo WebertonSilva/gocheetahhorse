b3c612b05f8b11649c43b324a914d342943c7923
Patricia dos Santos <psantosi@usersad.everis.int>
2017-02-16 09:52:14 -0200

    inclusão do bootstrap

--- gocheetahhorsefront/src/app/app.component.ts
+++ gocheetahhorsefront/src/app/app.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,15 +8,3 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app works!';
 }
-=======
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app works!';
-}
->>>>>>> b1145c0955e9ee62c4cb51dd7bed95e7042b6e58
--- gocheetahhorsefront/src/shared/header/header.component.ts
+++ gocheetahhorsefront/src/shared/header/header.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -14,20 +13,3 @@ export class HeaderComponent implements OnInit {
   }
 
 }
-=======
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
-})
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
->>>>>>> b1145c0955e9ee62c4cb51dd7bed95e7042b6e58
