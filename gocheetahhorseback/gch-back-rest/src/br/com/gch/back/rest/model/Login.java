b56ef89ff3cf53985cfa081c922662737758f7b4
malbacet <malbacet@SAO-7P982V1.usersad.everis.int>
2017-02-17 19:29:41 -0200

    Novas classes

--- gocheetahhorseback/gch-back-rest/src/br/com/gch/back/rest/model/Login.java
+++ gocheetahhorseback/gch-back-rest/src/br/com/gch/back/rest/model/Login.java
@@ -3,18 +3,32 @@ package br.com.gch.back.rest.model;
 public class Login {
 	
 	private String cpf;
+	private long dataNascimento;
+	private String codCia;
 
-	public Login(String cpf) {
-		super();
-		this.cpf = cpf;
+public Login() {}
+	
+	public long getDataNascimento() {
+		return this.dataNascimento;
+	}
+	
+	public void setDataNascimento(long dataNascimento) {
+		this.dataNascimento = dataNascimento;
 	}
 
 	public String getCpf() {
-		return cpf;
+		return this.cpf;
 	}
 
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+	
+	public String getCodCia() {
+		return this.codCia;
+	}
 
+	public void setCodCia(String origem) {
+		this.codCia = origem;
+	}
 }
